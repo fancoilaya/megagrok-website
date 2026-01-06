5a8e00635f514f17a5055b22c4ab12d99ee39139
fancoilaya <fancoilaya@gmail.com>
2026-01-06 09:36:31 +0100

    Create EnemyManager.ts

---
+++ game/arena/systems/EnemyManager.ts
@@ -0,0 +1,39 @@
+import Enemy from "../entities/Enemy";
+import * as Phaser from "phaser";
+
+export default class EnemyManager {
+  scene: Phaser.Scene;
+  enemies: Enemy[] = [];
+
+  constructor(scene: Phaser.Scene) {
+    this.scene = scene;
+  }
+
+  spawn(x: number, y: number) {
+    const enemy = new Enemy(this.scene, x, y);
+    this.enemies.push(enemy);
+  }
+
+  update(player: Phaser.Physics.Arcade.Sprite) {
+    this.enemies = this.enemies.filter(e => e.sprite.active);
+
+    for (const enemy of this.enemies) {
+      enemy.update(player);
+    }
+  }
+
+  damageAt(x: number, y: number, radius: number, dmg: number) {
+    for (const enemy of this.enemies) {
+      const d = Phaser.Math.Distance.Between(
+        x,
+        y,
+        enemy.sprite.x,
+        enemy.sprite.y
+      );
+
+      if (d <= radius) {
+        enemy.takeDamage(dmg);
+      }
+    }
+  }
+}
