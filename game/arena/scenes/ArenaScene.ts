1477b300fa965026dd99a484a1eceb67543e4ed5
fancoilaya <fancoilaya@gmail.com>
2026-01-06 09:38:57 +0100

    Update ArenaScene.ts

--- game/arena/scenes/ArenaScene.ts
+++ game/arena/scenes/ArenaScene.ts
@@ -1,8 +1,10 @@
 import * as Phaser from "phaser";
 import Player from "../entities/Player";
+import EnemyManager from "../systems/EnemyManager";
 
 export default class ArenaScene extends Phaser.Scene {
   player!: Player;
+  enemies!: EnemyManager;
 
   constructor() {
     super("ArenaScene");
@@ -16,18 +18,25 @@ export default class ArenaScene extends Phaser.Scene {
       .image(width / 2, height / 2, "arena-floor")
       .setDisplaySize(width, height);
 
-    // World bounds
     this.physics.world.setBounds(0, 0, width, height);
 
     // Player
     this.player = new Player(this, width / 2, height / 2);
 
+    // Enemies
+    this.enemies = new EnemyManager(this);
+
+    // Spawn a few test enemies
+    this.enemies.spawn(200, 200);
+    this.enemies.spawn(width - 200, 200);
+    this.enemies.spawn(width / 2, height - 200);
+
     // Camera
     this.cameras.main.startFollow(this.player.sprite, true, 0.08, 0.08);
-    this.cameras.main.setBounds(0, 0, width, height);
   }
 
-  update(_: number, delta: number) {
+  update(_, delta: number) {
     this.player.update(delta);
+    this.enemies.update(this.player.sprite);
   }
 }
