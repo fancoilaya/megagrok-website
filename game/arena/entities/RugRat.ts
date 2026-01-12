cbf76db2bbfd72101e828d6bfd1cae6f5f7126ca
fancoilaya <fancoilaya@gmail.com>
2026-01-12 14:27:31 +0100

    Update RugRat.ts

--- game/arena/entities/RugRat.ts
+++ game/arena/entities/RugRat.ts
@@ -3,8 +3,12 @@ import Enemy from "./Enemy";
 
 export default class RugRat extends Enemy {
   constructor(scene: Phaser.Scene, x: number, y: number) {
-    super(scene, x, y, "rugrat");
+    super(scene, x, y);
 
+    // Texture
+    this.sprite.setTexture("rugrat");
+
+    // Stats
     this.maxHp = 22;
     this.hp = this.maxHp;
     this.damage = 6;
