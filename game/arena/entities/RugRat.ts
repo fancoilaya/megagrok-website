ad0b16045c938f9bc29db665616bbf5f99605858
fancoilaya <fancoilaya@gmail.com>
2026-01-12 14:16:02 +0100

    Update RugRat.ts

--- game/arena/entities/RugRat.ts
+++ game/arena/entities/RugRat.ts
@@ -1,45 +1,30 @@
 import * as Phaser from "phaser";
+import Enemy from "./Enemy";
 
-export default class RugRat extends Phaser.Physics.Arcade.Sprite {
-  maxHp = 20;
-  hp = 20;
-  damage = 6;
-  speed = 170;
-
-  onDeath?: () => void;
-
-  constructor(scene: Phaser.Scene, x: number, y: number) {
-    super(scene, x, y, "rugrat");
-
-    scene.add.existing(this);
-    scene.physics.add.existing(this);
-
-    this.setCollideWorldBounds(true);
-  }
-
-  update(player: Phaser.Physics.Arcade.Sprite) {
-    if (!player.active) return;
-
-    const angle = Phaser.Math.Angle.Between(
-      this.x,
-      this.y,
-      player.x,
-      player.y
+export default class RugRat extends Enemy {
+  constructor(
+    scene: Phaser.Scene,
+    x: number,
+    y: number,
+    onKilled?: (points: number) => void
+  ) {
+    super(
+      scene,
+      x,
+      y,
+      "rugrat",
+      {
+        maxHp: 22,
+        damage: 6,
+        speed: 180,
+        defense: 1,
+        attackCooldown: 600,
+        contactDamage: 6,
+        killPoints: 8
+      },
+      onKilled
     );
 
-    this.scene.physics.velocityFromRotation(
-      angle,
-      this.speed,
-      (this.body as Phaser.Physics.Arcade.Body).velocity
-    );
-  }
-
-  takeDamage(amount: number) {
-    this.hp -= amount;
-
-    if (this.hp <= 0) {
-      this.onDeath?.();
-      this.destroy();
-    }
+    this.sprite.setScale(1);
   }
 }
