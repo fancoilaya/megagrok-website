cceacaf9c805f6044a42fa2127b4ae77610b87b6
fancoilaya <fancoilaya@gmail.com>
2026-01-12 14:15:33 +0100

    Update Croakling.ts

--- game/arena/entities/Croakling.ts
+++ game/arena/entities/Croakling.ts
@@ -1,45 +1,30 @@
 import * as Phaser from "phaser";
+import Enemy from "./Enemy";
 
-export default class Croakling extends Phaser.Physics.Arcade.Sprite {
-  maxHp = 12;
-  hp = 12;
-  damage = 4;
-  speed = 140;
-
-  onDeath?: () => void;
-
-  constructor(scene: Phaser.Scene, x: number, y: number) {
-    super(scene, x, y, "croakling");
-
-    scene.add.existing(this);
-    scene.physics.add.existing(this);
-
-    this.setCollideWorldBounds(true);
-  }
-
-  update(player: Phaser.Physics.Arcade.Sprite) {
-    if (!player.active) return;
-
-    const angle = Phaser.Math.Angle.Between(
-      this.x,
-      this.y,
-      player.x,
-      player.y
+export default class Croakling extends Enemy {
+  constructor(
+    scene: Phaser.Scene,
+    x: number,
+    y: number,
+    onKilled?: (points: number) => void
+  ) {
+    super(
+      scene,
+      x,
+      y,
+      "croakling",
+      {
+        maxHp: 14,
+        damage: 4,
+        speed: 150,
+        defense: 0,
+        attackCooldown: 700,
+        contactDamage: 4,
+        killPoints: 5
+      },
+      onKilled
     );
 
-    this.scene.physics.velocityFromRotation(
-      angle,
-      this.speed,
-      (this.body as Phaser.Physics.Arcade.Body).velocity
-    );
-  }
-
-  takeDamage(amount: number) {
-    this.hp -= amount;
-
-    if (this.hp <= 0) {
-      this.onDeath?.();
-      this.destroy();
-    }
+    this.sprite.setScale(0.9);
   }
 }
