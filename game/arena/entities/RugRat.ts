c92d939de78a61fa62aed96e1c59424e91c6f7e8
fancoilaya <fancoilaya@gmail.com>
2026-01-12 14:25:35 +0100

    Update RugRat.ts

--- game/arena/entities/RugRat.ts
+++ game/arena/entities/RugRat.ts
@@ -2,13 +2,8 @@ import * as Phaser from "phaser";
 import Enemy from "./Enemy";
 
 export default class RugRat extends Enemy {
-  constructor(
-    scene: Phaser.Scene,
-    x: number,
-    y: number,
-    onKilled?: (points: number) => void
-  ) {
-    super(scene, x, y, "rugrat", onKilled);
+  constructor(scene: Phaser.Scene, x: number, y: number) {
+    super(scene, x, y, "rugrat");
 
     this.maxHp = 22;
     this.hp = this.maxHp;
